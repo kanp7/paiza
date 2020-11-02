0bcd49deed2cdcc25d4c27dc512a5fc95328c1c3
kanp7 <tatsuya715tk@gmail.com>
2020-11-02 16:56:10 +0900

    additional

--- rankB_Up/DuplicateStringCount/Final.rb
+++ rankB_Up/DuplicateStringCount/Final.rb
@@ -1,3 +1,6 @@
+# 1行目で文字列 s、2行目で文字列 t が入力されます。
+# s が t の中で何回出現するかカウントして出力してください。
+
 s = gets.chomp
 t = gets.chomp
 
@@ -12,3 +15,10 @@ number_of_split.times do |i|
 end
 
 puts count
+
+# 補足
+# s = "AA"
+# t = "abdeeAAbAAAbfde"
+# t.scan(s).lengthの場合
+# AAAの部分が[0][1],[1][2]で判定されず回数が不足するためエラーとなる。
+# 結果2, 答え3
