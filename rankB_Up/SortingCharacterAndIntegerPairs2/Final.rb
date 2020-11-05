3ba9f24ee86dee9bfc9d80a6e450f4052e600c92
kanp7 <tatsuya715tk@gmail.com>
2020-11-05 17:29:24 +0900

    additonal

--- rankB_Up/SortingCharacterAndIntegerPairs2/Final.rb
+++ rankB_Up/SortingCharacterAndIntegerPairs2/Final.rb
@@ -20,3 +20,27 @@ sorted_list = list.invert.sort.reverse.to_h
 sorted_list.each do |integer, string|
     puts string + " " + integer.to_s
 end
+
+# 回答例2
+n = gets.to_i
+
+array = []
+n.times do
+    s, d = gets.chomp.split(" ")
+    array <<  [s, d.to_i]
+end
+# [["A", 1], ["D", 6], ["C", 2], ["G", 4], ["B", 70], ["A", 10], ["B", 5]]
+
+hash =  array.group_by(&:first).map{|k, v| [k, v.sum(&:last)]}.to_h
+# {"A"=>11, "D"=>6, "C"=>2, "G"=>4, "B"=>75}
+
+# list.group_by(&:first)部分の実行結果
+# {"A"=>[["A", 1], ["A", 10]], "D"=>[["D", 6]], "C"=>[["C", 2]], "G"=>[["G", 4]], "B"=>[["B", 70], ["B", 5]]}
+#.map部分の説明 | k=A,v=[["A", 1], ["A", 10]] | v（配列）の最後の要素、つまり数字を合計した値をvに代入している
+
+sort = hash.sort_by { |k, v| v }.reverse.to_h
+# {"B"=>75, "A"=>11, "D"=>6, "G"=>4, "C"=>2}
+
+sort.each do |key, value|
+  puts "#{key} #{value}"
+end
