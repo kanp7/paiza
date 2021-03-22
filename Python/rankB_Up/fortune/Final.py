4e985b5cde253bf146c9b4421a77783ed5b0b6b5
kanp7 <tatsuya715tk@gmail.com>
2021-03-22 23:34:38 +0900

    20210322

--- Python/rankB_Up/fortune/Final.py
+++ Python/rankB_Up/fortune/Final.py
@@ -23,3 +23,22 @@ for _ in range(n2):
 
 for k,v in dic.items():
     print(k + " " + dic2[v]) 
+
+# 回答例2
+from collections import OrderedDict
+def main():
+    n = int(input())
+    user_map = OrderedDict()
+    for _ in range(n):
+        user, blood_type = input().split()
+        user_map[user] = blood_type
+    m = int(input())
+    blood_type_map = OrderedDict()
+    for _ in range(m):
+        blood, fortune = input().split()
+        blood_type_map[blood] = fortune
+    for key, value in user_map.items():
+        blood_type = blood_type_map.get(str(value))
+        print("{} {}".format(key, blood_type))
+if __name__ == "__main__":
+    main()
