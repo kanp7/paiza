c61103ab111e1bcefa32c7eb0cdd504ac73253a0
kanp7 <tatsuya715tk@gmail.com>
2021-05-12 22:50:34 +0900

    20210512

---
+++ Python/LinearSearch/EnumerationOfHighAchievers/Final.py
@@ -0,0 +1,16 @@
+# n 人の生徒がテストを受けました。このテストで k 点以上 l 点以下の点数を取った人が合格です。
+
+# n 人の各生徒について、その人の名前とその人が取った点数が入力として与えられるので、このテストに合格した人の名前をすべて、入力された順に改行区切りで出力してください。
+
+# なお、合格者が一人もいない場合は、何も出力しないでください。
+
+n = int(input())
+dic = {}
+for _ in (range(n)):
+    name, point = input().split()
+    dic[name] = int(point)
+k, l = list(map(int,input().split()))
+
+for name, point in dic.items():
+    if point >= k and point <= l:
+        print(name)
