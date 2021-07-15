4a7ad3054bbda1beb24b627e5d444724803bf02c
kanp7 <tatsuya715tk@gmail.com>
2021-07-15 23:29:51 +0900

    20210715

--- Python/Query/SortingAndSearching/step3.py
+++ Python/Query/SortingAndSearching/step3.py
@@ -1,6 +1,7 @@
 # 長さ N の重複した要素の無い数列 A と Q 個の整数 K_1 ... K_Q が与えられるので、
 # 各 K_i について、 A に K_i が含まれていれば "YES" を、そうでなければ "NO" を出力してください。
 
+# n=98765 q=100000 のとき時間切れ
 n, q = list(map(int,input().split()))
 
 numbers = []
@@ -14,4 +15,16 @@ for _ in range(q):
     else:
         print("NO")
 
-# n=98765 q=100000 のとき時間切れ
+# listの代わりに、setを使用することで時間内に処理が可能になる
+n, q = list(map(int,input().split()))
+
+numbers = set()
+for _ in range(n):
+    numbers.add(int(input()))
+
+for _ in range(q):
+    k = int(input())
+    if k in numbers:
+        print("YES")
+    else:
+        print("NO")
