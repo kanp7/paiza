7b4a5e17b5cf8deb38b7ba3d4f24193eb4123502
kanp7 <tatsuya715tk@gmail.com>
2021-07-16 23:27:18 +0900

    20210716

--- Python/Query/SquareCut/step3.py
+++ Python/Query/SquareCut/step3.py
@@ -15,3 +15,38 @@ for _ in range(h):
 
 for _ in range(n):
     x,y = list(map(int,input().split()))
+
+# 解答
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int H, W, N;
+  cin >> H >> W >> N;
+  int A[H][W], sum[H][W];
+
+  for (int i = 0; i < H; i++) {
+    for (int j = 0; j < W; j++) {
+      cin >> A[i][j];
+      sum[i][j] = A[i][j];
+      if (0 < i) {
+        sum[i][j] += sum[i - 1][j];
+      }
+      if (0 < j) {
+        sum[i][j] += sum[i][j - 1];
+      }
+      if (0 < i && 0 < j) {
+        sum[i][j] -= sum[i - 1][j - 1];
+      }
+    }
+  }
+
+  for (int i = 0; i < N; i++) {
+    int y, x;
+    cin >> y >> x;
+    y--;
+    x--;
+    cout << sum[y][x] << endl;
+  }
+}
