b2b2bcd67c6d938378ad00e3f0ac7bd92dbeee77
kanp7 <tatsuya715tk@gmail.com>
2021-10-11 23:18:05 +0900

    20211011

---
+++ Python/rankB_Up/SortingCharacterAndIntegerPairs/step4.py
@@ -0,0 +1,23 @@
+# 1行目で整数 n が与えられ、2行目で n 個の整数が与えられます。
+# n 個の整数を昇順に出力してください。
+
+n = int(input())
+nums = list(map(int,input().split()))
+sort_nums = sorted(nums)
+
+for n in sort_nums:
+    print(n)
+
+
+# 解答2
+input()  # 1つ目の入力は使わないので読み飛ばす
+strings = input().split()
+nums = []
+
+for num in strings:
+    nums.append(int(num))
+
+nums.sort()
+
+for i in nums:
+    print(i)
