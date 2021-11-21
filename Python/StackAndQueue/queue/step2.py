48ad2470ca9b87454f37db38c2386ed10fb8384b
kanp7 <tatsuya715tk@gmail.com>
2021-11-21 20:44:18 +0900

    20211121

---
+++ Python/StackAndQueue/queue/step2.py
@@ -0,0 +1,16 @@
+# Q 個のクエリが与えられます。空の配列 A を用意したあと、 Q 個のクエリに応じて以下の 2 種類の処理をしてください。
+
+# ・ PUSH X: 配列 A の末尾に 文字 X を追加
+# ・ POP: 配列 A の先頭にある要素を出力し、削除
+
+# 各クエリの処理が終わったあと、配列 A の各要素の値を半角スペース区切りで出力してください。
+
+n = int(input())
+queue = []
+for _ in range(n):
+    query = input().split()
+    if query[0] == '1':
+        queue.append(query[1])
+    else:
+        print(queue.pop(0))
+    print(" ".join(queue))
